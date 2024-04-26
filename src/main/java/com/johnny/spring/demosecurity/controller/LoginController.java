42c08393e6c966ea4d34cf0015036f54bf3b5eb3
johnny <john.robertson88@yahoo.com>
2024-04-25 23:27:27 -0700

    Create a cutom access denied page and set the security exception handling to forward users to that page when they attempt to access a page they are not authorized to access

--- src/main/java/com/johnny/spring/demosecurity/controller/LoginController.java
+++ src/main/java/com/johnny/spring/demosecurity/controller/LoginController.java
@@ -8,7 +8,12 @@ public class LoginController {
 
     @GetMapping("/showMyLoginPage")
     public String showMyLoginPage(){
-        return "fancy-login";
 //        return "plain-login";
+        return "fancy-login";
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied(){
+        return "access-denied";
     }
 }
